3b1c2fdc3871316ab8ac5d4a36e3611c40af95f7
Natalie Dean <ndean87@gmail.com>
2017-08-23 15:59:11 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,29 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_hash = {}
+  data.each do |pigeon_attributes, pigeon_data|
+    pigeon_data.each do |pigeon_descripters, pigeon_name_array|
+      pigeon_name_array.each do |name|
+        if !pigeon_hash[name]
+          pigeon_hash[name] = {
+            :color => [],
+            :gender => [],
+            :lives => []
+          }
+        end
+        if
+          pigeon_hash[name][pigeon_attributes] << pigeon_descripters.to_s
+        end
+      end
+    end
+  end
+  # data.each do |pigeon_attributes, pigeon_data|
+  #   pigeon_data.each do |details, names|
+  #     names.each do |name|
+  #       pigeon_hash[name][pigeon_attributes] << details
+  #     end
+  #   end
+  # end
+  return pigeon_hash
 end
